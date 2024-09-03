3e0d1e1915a49e8fb870fdaf08288f11b4da03de
Nallannagari Reddaiah <reddaiahnallannagari@gmail.com>
2024-09-04 00:01:17 +0530

    1.Auth.controllers added

---
+++ backend/models/user_model.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+
+const userSchema =new mongoose.Schema({
+    //Timestamps is used to note the time of the user when he is logged in
+    //creates the account it self
+    //member since july 2021 createdAt
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    followers:[
+        {
+            type:mongoose.Schema.Types.ObjectId, //Reference to the user model
+            //16 characters
+            ref:"User",//reference to the user model
+            default:[]
+        }
+    ],
+    following:[
+        {
+            type:mongoose.Schema.Types.ObjectId, //Reference to the user model
+            //16 characters
+            ref:"User",//reference to the user model
+            default:[]
+        }
+    ],
+    profileImage:{
+        type:String,
+        default:"",
+    },
+    coverImg:{
+        type:String,
+        default:"",
+    },
+    Bio:{
+        type:String,
+        default:"",
+    },
+    link:{
+        type:String,
+        default:"",
+    }
+},{timestamps:true})
+
+const User=mongoose.model("User",userSchema);
+//Name of teh collection in the mongoose is going to be users collection
+export default User;
